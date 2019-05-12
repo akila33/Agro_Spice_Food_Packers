6ad8e2fe2bc20b70515338f74aafa3bb4c2443ff
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-05-12 14:58:48 +0530

    Notification service backend success

--- client/src/app/notifications.service.ts
+++ client/src/app/notifications.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class NotificationsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  api='http://localhost:3030/api/sendMail';
+
+  sendEmail(data){
+    return this.http.post(this.api,data);
+  }
+
 }
