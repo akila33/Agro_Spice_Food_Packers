52dcd02e66c1199062ba78e0887e6d5267633676
akila33 <akila.rangoda@gmail.com>
2019-05-21 19:47:02 +0530

    client homepage UI modified

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,10 @@
 
 
 //including required services and modules 
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from './data.service';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 //Component specific details 
 @Component({
@@ -18,9 +19,16 @@ export class AppComponent {
   searchTerm = '';
   isCollapsed = true;
 
-  constructor(private router: Router, private data: DataService) {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+
+  
+
+  constructor(private router: Router, private data: DataService,public location: Location, private element : ElementRef) {
     this.data.cartItems = this.data.getCart().length;
     this.data.getProfile();
+
+    this.sidebarVisible = false;
   }
 
   get token() {
@@ -52,4 +60,127 @@ export class AppComponent {
   getShortName(fullName) { 
     return fullName.split(' ')[0];
   }
+
+
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
 }
+sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    // console.log(toggleButton, 'toggle');
+
+    setTimeout(function(){
+        toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+};
+sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+};
+sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+        this.sidebarOpen();
+    } else {
+        this.sidebarClose();
+    }
+};
+isHome() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+
+    if( titlee === '/home' ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+isDocumentation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if( titlee === '/documentation' ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+}
+
+
+
+
+
+
+
+
+// export class NavbarComponent implements OnInit {
+//   private toggleButton: any;
+//   private sidebarVisible: boolean;
+
+//   constructor(public location: Location, private element : ElementRef) {
+//       this.sidebarVisible = false;
+//   }
+
+//   ngOnInit() {
+//       const navbar: HTMLElement = this.element.nativeElement;
+//       this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+//   }
+//   sidebarOpen() {
+//       const toggleButton = this.toggleButton;
+//       const html = document.getElementsByTagName('html')[0];
+//       // console.log(html);
+//       // console.log(toggleButton, 'toggle');
+
+//       setTimeout(function(){
+//           toggleButton.classList.add('toggled');
+//       }, 500);
+//       html.classList.add('nav-open');
+
+//       this.sidebarVisible = true;
+//   };
+//   sidebarClose() {
+//       const html = document.getElementsByTagName('html')[0];
+//       // console.log(html);
+//       this.toggleButton.classList.remove('toggled');
+//       this.sidebarVisible = false;
+//       html.classList.remove('nav-open');
+//   };
+//   sidebarToggle() {
+//       // const toggleButton = this.toggleButton;
+//       // const body = document.getElementsByTagName('body')[0];
+//       if (this.sidebarVisible === false) {
+//           this.sidebarOpen();
+//       } else {
+//           this.sidebarClose();
+//       }
+//   };
+//   isHome() {
+//       var titlee = this.location.prepareExternalUrl(this.location.path());
+
+//       if( titlee === '/home' ) {
+//           return true;
+//       }
+//       else {
+//           return false;
+//       }
+//   }
+//   isDocumentation() {
+//       var titlee = this.location.prepareExternalUrl(this.location.path());
+//       if( titlee === '/documentation' ) {
+//           return true;
+//       }
+//       else {
+//           return false;
+//       }
+//   }
+// }
